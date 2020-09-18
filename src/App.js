7b3c9f893af59ebb6a528562fb815bd1353bb52d
GusBedasi <ggustavobedasi@gmail.com>
2020-09-18 01:50:15 -0300

    Adicionado fetch do dados de clima com a API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
 import WeatherCard from './components/WeatherCard/WeatherCard';
 
@@ -12,6 +13,10 @@ function App() {
   const [ city, setCity ] = useState('')
   const [ active, setActive ] = useState(false)
 
+  const [fullWeatherInfo, setFullWeatherInfo] = useState({cityName: '', temperature: '', description: '', max: '', min: ''})
+
+  const apiKey = '51cd2bf414388469853b02b79ee72d04'
+
   // Input handler function
   const inputHandler = (input) => {    
     setSearchTerm(input.target.value)
@@ -24,6 +29,28 @@ function App() {
     setActive(true)
   }
 
+  const fahrenheitConversor = (temp) => {
+    return  Math.floor(temp - 273.15)
+  }
+
+  useEffect(() => {
+    async function fetchData(){
+      const response = await (await axios.get(`//api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)).data
+      
+      const cityName = response.name
+      const temperature = fahrenheitConversor(response.main.temp)
+      const description = response.weather[0].description
+      const max = fahrenheitConversor(response.main.temp_max)
+      const min = fahrenheitConversor(response.main.temp_min)
+
+      setFullWeatherInfo({...fullWeatherInfo, cityName, temperature, description, max, min})
+    }
+
+    if(active){
+      fetchData()
+    }
+  }, [city])
+
   return (
     <div className="App">
       <div className="search-block">
@@ -32,7 +59,7 @@ function App() {
         <button type="submit" onClick={submitHandler}>Search</button>
       </div>
       <div className={ active ? 'weather-block' : 'display-none' }>
-        <WeatherCard city={city} temperature={'30 C'} description={'Pouco nublado'} max={'31 C'} min={'16 C'}></WeatherCard>
+        <WeatherCard city={fullWeatherInfo.cityName} temperature={fullWeatherInfo.temperature + ' C'} description={fullWeatherInfo.description} max={fullWeatherInfo.max + ' C'} min={fullWeatherInfo.min + ' C'}></WeatherCard>
       </div>
     </div>
   );
